5d7fbaead0136ddd87b232bb459e598d66ef3e12
saurabhjadhv <saurabhjadhav150@gmail.com>
2024-03-16 21:05:51 +0530

    cha

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login', [UserController::class, 'login']);
 Route::post('/register', [UserController::class, 'register']);
